7e36dbf1c3c7159f225bbbd4f53cadc0339d4711
Ricardo <ricardo_brito_santos@hotmail.com>
2021-06-16 21:00:55 -0300

    Resolvido

--- src/Temperatura.java
+++ src/Temperatura.java
@@ -1,9 +1,22 @@
+//Escreva um programa que, com base em uma temperatura em graus celsius, a converta e exiba em Kelvin (K), Réaumur (Re), Rankine (Ra) e Fahrenheit (F), seguindo as fórmulas: F = C * 1.8 + 32; K = C + 273.15; Re = C * 0.8; Ra = C * 1.8 + 32 + 459.67
+
+import java.util.Scanner;
 
 public class Temperatura {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		var leitor = new Scanner(System.in);
+		System.out.println("Digite a temperatura em Celsius");
+		var temperaturaCelsius = leitor.nextDouble();
+		
+		System.out.println("A temperatura:" + temperaturaCelsius + "ºC");
+		System.out.println("É equivalente a: " + (temperaturaCelsius + 273.15) + "K");
+		System.out.println("É equivalente a: " + (temperaturaCelsius * 0.8) + "Re");
+		System.out.println("É equivalente a: " + (temperaturaCelsius * 1.8 + 32 + 459.67) + "Ra");
+		System.out.println("É equivalente a: " + (temperaturaCelsius * 1.8 + 32) + "F");
+		
+		leitor.close();
 	}
 
 }
